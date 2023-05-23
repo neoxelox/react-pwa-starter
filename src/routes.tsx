277db46bdddeda8ef0238845656c5ad2eea48847
Alex <alex@neoxelox.com>
2023-05-23 19:49:49 +0200

    Simplify router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import App from "./App.tsx";
 import Error from "./Error.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  /* The 404 page must be the last route */
-  {
-    path: "*",
-    element: <Error />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<App />} />
+      {/* The 404 page must be the last route */}
+      <Route path="*" element={<Error />} />
+    </Route>,
+  ),
+);
 
 export default function Router() {
   return <RouterProvider router={router} />;
