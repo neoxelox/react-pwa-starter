68e8b9d1b993ae6eb563873e8095bbeeab12b349
Alex <me@arn.sh>
2023-09-28 14:53:43 +0200

    Update dependencies

--- .tailwindrc.cjs
+++ .tailwindrc.cjs
@@ -24,10 +24,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio")],
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./main.scss";
 import Router from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
